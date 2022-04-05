d09f63fef6614e43f92e8dc3b1392e6e86930423
anilpandey071999 <31802461+anilpandey071999@users.noreply.github.com>
2022-04-05 21:39:50 +0530

    WhiteList

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,100 +1,119 @@
-import React, { useEffect, useState, useRef } from 'react';
-import '../App.css';
-import './HeroSection.css';
-import BG from '../img/BG.png';
-import logo from '../img/logo.png';
-import Discord from '../img/Discord.png';
-import Instagram from '../img/Instagram.png';
-import Twitter from '../img/Twitter.png';
-import Grid from '@material-ui/core/Grid';
+import React, { useEffect, useState, useRef } from "react";
+import "../App.css";
+import "./HeroSection.css";
+import BG from "../img/BG.png";
+import logo from "../img/logo.png";
+import Discord from "../img/Discord.png";
+import Instagram from "../img/Instagram.png";
+import Twitter from "../img/Twitter.png";
+import Grid from "@material-ui/core/Grid";
 import ReactDOM from "react-dom";
-import {GroupedButtons,slotvalue}  from "./GroupedButtons";
-import 'aos/dist/aos.css';
-import { Button } from '@material-ui/core';
+import { GroupedButtons, slotvalue } from "./GroupedButtons";
+import "aos/dist/aos.css";
+import { Button } from "@material-ui/core";
 const { ethers } = require("ethers");
 const Web3 = require("web3");
-const bbcc = require('./pages/abi/abi.json')
-
+const bbcc = require("./pages/abi/abi.json");
 
 function HeroSection() {
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
   const [accounts, setAccount] = useState("");
-  const [chainID, setChainID] = useState('137');
-  const [a,seta] = useState("0.1")
+  const [chainID, setChainID] = useState("137");
+  const [a, seta] = useState("0.1");
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
-  const [ts , setts]= useState("")
-  
-  
-  
-  useEffect(()=>{
-    seta(((0.1)*slotvalue).toString())
-  })
+  const [ts, setts] = useState("");
+  const [whiteListType, setwhiteListType] = useState("n");
 
-  const checkforwhitelist = async ( useraddress) =>{
-    try { if(useraddress ===0){
-      alert(" get your metamask bitch");}
-     else{ 
-       
-    let contractAddress = "0x0c60447Ce83877C19c52B8E876D194D00f68b30e";
-     const provider = new ethers.providers.Web3Provider(window.ethereum);
-     const signer = provider.getSigner();
-     const bc = new ethers.Contract(contractAddress, bbcc, signer);
-     let bb= await bc.whiteListVIPMint(t1.address)
-     let v1 = await bc.whiteListVIPMintLimit(t1.address)
+  useEffect(() => {
+    seta((0.1 * slotvalue).toString());
+  });
 
-      let cc = await bc.whiteListPreMint(t3.address)
-      let v2 = await bc.whiteListPreMintLimit(t3.address)
-
-      let dd = await bc.normalWhiteListing(t2.address)
-      let v3 = await bc.normalWhiteListingLimit(t2.address)
-      }
-     
-
-    }catch(error){
-      console.log(error)
-    }
-  };
-  
   const connetMetMask = async () => {
     try {
-      console.log('slot id :',slotvalue);
+      console.log("slot id :", slotvalue);
       const [accounts] = await window.ethereum.request({
         method: "eth_requestAccounts",
       });
       setAccount(accounts);
-      const chainID1 = await window.ethereum.request({ method: 'eth_chainId' })
-      console.log('chainid',chainID1)
+      const chainID1 = await window.ethereum.request({ method: "eth_chainId" });
+      console.log("chainid", chainID1);
       setChainID(chainID1);
       let contractAddress = "0x0c60447Ce83877C19c52B8E876D194D00f68b30e";
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
       const BBCCContract = new ethers.Contract(contractAddress, bbcc, signer);
-      const totalsuppuly = await BBCCContract.totalSupply()
-      // funtion for checking to whitelist 
+      const totalsuppuly = await BBCCContract.totalSupply();
+      // funtion for checking to whitelist
+      let whiteListVIPMint = await BBCCContract.whiteListVIPMint(accounts);
+      let whiteListVIPMintLimit = await BBCCContract.whiteListVIPMintLimit(
+        accounts
+      );
+
+      let whiteListPreMint = await BBCCContract.whiteListPreMint(accounts);
+      let whiteListPreMintLimit = await BBCCContract.whiteListPreMintLimit(
+        accounts
+      );
+
+      let normalWhiteListing = await BBCCContract.normalWhiteListing(accounts);
+      let normalWhiteListingLimit = await BBCCContract.normalWhiteListingLimit(
+        accounts
+      );
+      // checkforwhitelist();
+
+      if (whiteListVIPMint) await setwhiteListType("whiteListVIPMint");
+      if (whiteListPreMint) await setwhiteListType("whiteListPreMint");
+      if (normalWhiteListing) await setwhiteListType("normalWhiteListing");
+      if (whiteListVIPMint) console.log("whiteListVIPMint");
+      if (whiteListPreMint) console.log("whiteListPreMint");
+      if (normalWhiteListing) console.log("normalWhiteListing");
+      // : setwhiteListType("FUCK");
+
+      console.log("whiteListType:-", whiteListType);
 
       console.log(totalsuppuly.toString());
       setts(totalsuppuly.toString());
-      console.log("current account",accounts)
+      console.log("current account", accounts);
     } catch (error) {
       console.log(error);
     }
   };
+
   const mintNFT = async (_slotID) => {
     try {
-      let val = ethers.utils.parseEther("0.1"*_slotID);
+      console.log("whiteListType:-", whiteListType);
 
       let contractAddress = "0x0c60447Ce83877C19c52B8E876D194D00f68b30e";
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const signer = provider.getSigner();
-        const BBCCContract = new ethers.Contract(contractAddress, bbcc, signer);
-       
-
-        let Tx = await BBCCContract.mintSuperCar(_slotID, {value: ethers.utils.parseEther("0.1")});
-        console.log("tx mintied");
-        Tx.wait();
-        console.log(Tx);
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const BBCCContract = new ethers.Contract(contractAddress, bbcc, signer);
+      let Tx;
+      // let val = ethers.utils.parseEther("0.1" * _slotID);
+      if (whiteListType === "whiteListVIPMint") {
+        Tx = await BBCCContract.mintSuperCar(_slotID, {
+          value: ethers.utils.parseEther(`${0.06 * _slotID}`),
+        });
+      }
+      if (whiteListType === "whiteListPreMint") {
+        Tx = await BBCCContract.mintSuperCar(_slotID, {
+          value: ethers.utils.parseEther(`${0.05 * _slotID}`),
+        });
+      }
+      if (whiteListType === "normalWhiteListing") {
+        Tx = await BBCCContract.mintSuperCar(_slotID, {
+          value: ethers.utils.parseEther(`${0.1 * _slotID}`),
+        });
+      }
+      if (whiteListType === "") {
+        Tx = await BBCCContract.mintSuperCar(_slotID, {
+          value: ethers.utils.parseEther(`${0.1 * _slotID}`),
+        });
+      }
+      console.log("tx mintied:- ", Tx.hash);
+      alert(Tx.hash);
+      Tx.wait();
+      console.log(Tx);
     } catch (error) {}
   };
 
@@ -112,75 +131,76 @@ function HeroSection() {
 
   window.addEventListener("resize", showButton);
 
-
   return (
     <>
-      <div id='herosection' className='hero-container'>
+      <div id="herosection" className="hero-container">
         <Grid container spacing={0}>
+          <Grid item xs={4} md={4} sm={4}></Grid>
           <Grid item xs={4} md={4} sm={4}>
-          </Grid>
-          <Grid item xs={4} md={4} sm={4}>
-            <div className='logo'>
-              <img className='logoImg' src={logo} />
+            <div className="logo">
+              <img className="logoImg" src={logo} />
             </div>
           </Grid>
           <Grid item xs={4} md={4} sm={4}>
-            <div className='walletButton'>
-                 <button className="wallet" onClick={() => connetMetMask()} >
+            <div className="walletButton">
+              <button className="wallet" onClick={() => connetMetMask()}>
                 {accounts.length > 0
                   ? `${accounts[0]}${accounts[1]}${accounts[2]}${accounts[3]}${accounts[4]}...${accounts[30]}${accounts[31]}${accounts[32]}`
                   : "Connect Wallet"}
-              
               </button>
             </div>
           </Grid>
         </Grid>
 
-        <div className='introHead'>
-          <h1 className='intro1'>
-            Ladies and Gentlemen!
-          </h1>
-          <h1 className='intro2'>
-            START YOUR ENGINES
+        <div className="introHead">
+          <h1 className="intro1">Ladies and Gentlemen!</h1>
+          <h1 className="intro2">START YOUR ENGINES</h1>
+        </div>
+        <div className="containerBox">
+          <h1 className="tokenminted1">
+            Tokens Minted : <span style={{ color: "#2C9B1A" }}>&nbsp;{ts}</span>
+            /8000
           </h1>
         </div>
-        <div className='containerBox'>
-          <h1 className='tokenminted1'>Tokens Minted : <span style={{ color: '#2C9B1A' }}>&nbsp;{ts}</span>/8000</h1>
-         
-        </div>
-        <div className='quantityButton'>
+        <div className="quantityButton">
           <GroupedButtons />
         </div>
-        <div className='mintButton'>
-          <Button className='mint' onClick={() =>mintNFT(slotvalue)}>Mint Now</Button>
+        <div className="mintButton">
+          <Button className="mint" onClick={() => mintNFT(slotvalue)}>
+            Mint Now
+          </Button>
         </div>
-        <div className='disclaimer'>
-          <h1 className='dis-txt'>disclaimer : </h1>
+        <div className="disclaimer">
+          <h1 className="dis-txt">disclaimer : </h1>
         </div>
-        <div className='footer'>
+        <div className="footer">
           <Grid container spacing={0}>
             <Grid item xs={4} md={4} sm={4}>
-              <div className='imgBox'>
-                <a ><img className='img-footer' src={Discord} /></a>
+              <div className="imgBox">
+                <a>
+                  <img className="img-footer" src={Discord} />
+                </a>
               </div>
             </Grid>
             <Grid item xs={4} md={4} sm={4}>
-              <div className='imgBox'>
-                <a ><img className='img-footer' src={Instagram} /></a>
+              <div className="imgBox">
+                <a>
+                  <img className="img-footer" src={Instagram} />
+                </a>
               </div>
             </Grid>
             <Grid item xs={4} md={4} sm={4}>
-              <div className='imgBox'>
-                <a><img className='img-footer' src={Twitter} /></a>
+              <div className="imgBox">
+                <a>
+                  <img className="img-footer" src={Twitter} />
+                </a>
               </div>
             </Grid>
           </Grid>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/web3@latest/dist/web3.min.js"></script>
-
       </div>
     </>
-
   );
 }
 
